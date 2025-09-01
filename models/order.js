53e0ba135f8ac4a3ec257251cc8c330c1b1b597b
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-09-01 19:59:11 +0300

    payment status

--- models/order.js
+++ models/order.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  quantity: Number,
-});
+const productSchema = new mongoose.Schema({ name: String, price: Number,  quantity: Number });
 
 const orderSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   products: [productSchema],
   date: { type: Date, default: Date.now },
   total: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["refused", "canceled", "accepted", "shipping", "delivered"],
+    default: "pending"
+  },
+    payment: {type: String, default: "cash on delivery"}
 });
-
 
 module.exports = mongoose.model("Order", orderSchema);
