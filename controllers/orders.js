d1d7de4eedfe977e6f41adbc1ab4db1803a17c62
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-09-02 02:52:47 +0300

    improvements

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,15 +1,10 @@
 const Order = require("../models/order");
 
-// Create new order
 exports.createOrder = async (req, res) => {
-  const { userId, products, date, total } = req.body;
-
-  if (!userId || !products || !date || total == null) {
-    return res.status(400).json({ error: "Missing fields" });
-  }
-
+  const { userId, products, date, total,payment } = req.body;
+  if (!userId || !products || !date || total == null) return res.status(400).json({ error: "Missing fields" });
   try {
-    const newOrder = new Order({ userId, products, date, total });
+    const newOrder = new Order({ userId, products, date, total, payment });
     await newOrder.save();
     res.status(201).json({ message: "Order saved" });
   } catch (err) {
@@ -18,7 +13,6 @@ exports.createOrder = async (req, res) => {
   }
 };
 
-// Get orders by user
 exports.getUserOrders = async (req, res) => {
   const { userId } = req.params;
 
