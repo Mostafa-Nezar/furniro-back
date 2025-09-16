1a6e4ffdc654163344d6ec850c7706391a87f631
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-09-16 22:48:03 +0300

    payment 2

--- controllers/payment2.js
+++ controllers/payment2.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_2);
 
 exports.createPaymentIntent = async (req, res) => {
   try {
--- routes/webhook2.js
+++ routes/webhook2.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 const { handleStripeWebhook2 } = require("../controllers/webhook2"); 
 
-router.post("/", handleStripeWebhook2);
+router.post("/", express.raw({ type: "application/json" }), handleStripeWebhook2);
 
 module.exports = router;
