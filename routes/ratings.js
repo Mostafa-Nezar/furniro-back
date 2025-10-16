2dc6a959b28987ffe02b6e00a35de8dba08565e0
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-10-16 15:17:44 +0300

    back to web

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getAllRatings, addRating, getTopRatingsWithUsers } = require("../controllers/ratings");
+const { getAllRatings, addRatingx, getTopRatingsWithUsers } = require("../controllers/ratings");
 
 router.get("/", getAllRatings);
-router.post("/", addRating);
+router.post("/", addRatingx);
 router.get("/top-ratings/:productId", getTopRatingsWithUsers); 
 module.exports = router;
