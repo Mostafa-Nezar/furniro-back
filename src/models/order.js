e1c199963fca205674541b18c07fe9824f71f9bc
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-01-20 05:51:38 +0200

    finish cleaning cart

--- src/models/order.js
+++ src/models/order.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({id:Number, name: String, price: Number, quantity: Number, size:String, color:String });
+const productSchema = new mongoose.Schema({id:Number, name: String, price: Number, quantity: Number, size:String, color:String, cartItemId:String, });
 const customerInfoSchema = new mongoose.Schema({ fullName: String, email: String, address: String, city: String, state: String, zipCode: String, phoneNumber: Number });
 const orderSchema = new mongoose.Schema({
   userId: { type: String, required: true },
