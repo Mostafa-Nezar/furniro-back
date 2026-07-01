aad9bd0e381a904b74bb4ef840e537db9e1bf283
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-07-01 12:03:19 +0300

    hi server

--- src/validators/rating.js
+++ src/validators/rating.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
 exports.addRatingSchema = Joi.object({
-    userid: Joi.number().integer().optional(),
+    userid: Joi.number().integer().required(),
     userref: Joi.string().optional(),
     productid: Joi.number().integer().required(),
     productref: Joi.string().optional(),
     rateid: Joi.string().required(),
     rate: Joi.number().integer().min(0).max(5).required(),
-    comment: Joi.string().max(1000).optional().allow('', null)
-}).unknown(true);
+    comment: Joi.string().max(1000).optional()
+});
 
