eeb2a55d450d5257b93511df46a5601b8ff6f83e
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-07-27 16:52:56 +0300

    x2

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -2,11 +2,13 @@ const Joi = require('joi');
 
 exports.signinSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
+    deviceInfo: Joi.object().unknown(true).optional()
 });
 
 exports.googleSignInSchema = Joi.object({
-    token: Joi.string().required()
+    token: Joi.string().required(),
+    deviceInfo: Joi.object().unknown(true).optional()
 });
 
 exports.updateLocationSchema = Joi.object({
