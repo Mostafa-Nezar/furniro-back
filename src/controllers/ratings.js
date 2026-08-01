229477d1a9af87d9e56b68fe3a77784f25592939
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-08-02 01:36:10 +0300

    nazarify website

--- src/controllers/ratings.js
+++ src/controllers/ratings.js
@@ -77,9 +77,9 @@ exports.getTopRatingsWithUsers = async (req, res) => {
 
 exports.addRatingtest = async (req, res) => {
   try {
+    const { productid, rate, comment } = req.body;
     const authenticatedUserId = req.user.id;
     const rateid = `${authenticatedUserId}-${productid}`;
-    const { productid, rate, comment } = req.body;
     const user = await User.findOne({ id: authenticatedUserId });
     if (!user) {
       return res.status(401).json({ msg: "Unauthorized: User not found" });
