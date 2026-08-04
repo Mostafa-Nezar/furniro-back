c14245dadbdf477e2894e3d06600032f24b5af90
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-08-05 01:02:22 +0300

    fix admin login

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -63,8 +63,8 @@ exports.loginAdmin = async (req, res) => {
     res.cookie("adminToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+      sameSite: "none",
+      maxAge: 30* 24 * 60 * 60 * 1000 // 7 days
     });
 
     res.json({
